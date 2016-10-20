676e4e516136c13f5deb2411d21d09b300da0712
Tim <runcodd@gmail.com>
2016-10-20 22:27:11 +0200

    add more code)

--- js/common.js
+++ js/common.js
@@ -1,5 +1,26 @@
 $(function() {
 
-	// $("body").hide();
+
+  $(".burger").on("click",function () {
+    $(".nav-main").slideToggle();
+  });
+
+$(function() {
+        // Call on every window resize
+        $(window).resize(function(){
+            if ($(window).width() < 1025 ) {
+              $(".nav-main ul li").on("click",function (e) {
+                if(!$(e.target).closest("div").is(".nav-main__sub")) {
+                  $(".nav-main__sub",this).toggle();
+                  $(this).siblings(".nav-main ul li").find(".nav-main__sub").hide();
+                }
+              });
+            } else {
+                // return false;   // <<< whatever the other margin value should be goes here
+            }
+        }).resize();   // Cause an initial widow.resize to occur
+    });
+
+
 
 });
